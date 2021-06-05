c7aeeeb76f55f62dc73f36293be4c5a920dbd0a0
stsuchi <shiro.tchya@gmail.com>
2021-06-05 17:31:35 +0900

    update discription for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='Japanese-BPE-Tokenizer',
       version='0.1',
       description='Mecab-based BPE toknizer for Japanese text',
@@ -8,4 +13,6 @@ setup(name='Japanese-BPE-Tokenizer',
       license='MIT',
       packages=['jpn_bpe_tokenizer'],
       install_requires=['transformers','fugashi','unidic_lite'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       zip_safe=False)
